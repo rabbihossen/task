9f8ae13589e8f7795afef0bb54a2132de246e382
rabbihossen <joyahmedauthor@gmail.com>
2022-04-17 23:41:50 +0600

    show dialog

--- lib/widget/product_widget.dart
+++ lib/widget/product_widget.dart
@@ -10,61 +10,105 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-                height: 90,
-                width: 100,
-                child: Image.network(
-                  product.image,
-                  fit: BoxFit.fill,
-                )),
-            Text(product.title,
+    return InkWell(
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                  height: 90,
+                  width: 100,
+                  child: Image.network(
+                    product.image,
+                    fit: BoxFit.fill,
+                  )),
+              Text(product.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 12)),
+              Text(
+                "Category: ${product.category.name}",
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 12)),
-            Text(
-              "Category: ${product.category.name}",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(color: Colors.grey, fontSize: 11),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(
-                    flex: 3,
-                    child: Text(
-                      "৳ ${product.price} ",
-                      style: TextStyle(
-                          color: primarycolor,
-                          fontSize: 11,
-                          fontWeight: FontWeight.bold),
-                    )),
-                Expanded(
-                    flex: 3,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Container(
-                        height: 20,
-                          color: primarycolor,
-                          child: Center(
-                              child: Text(
-                            "add to cart",
-                            style: TextStyle(color: Colors.white, fontSize: 10),
-                          ))),
-                    ))
-              ],
-            )
-          ],
+                style: const TextStyle(color: Colors.grey, fontSize: 11),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                      flex: 3,
+                      child: Text(
+                        "৳ ${product.price} ",
+                        style: TextStyle(
+                            color: primarycolor,
+                            fontSize: 11,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  Expanded(
+                      flex: 3,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Container(
+                            height: 20,
+                            color: primarycolor,
+                            child: Center(
+                                child: Text(
+                              "add to cart",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 10),
+                            ))),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(20.0)), //this right here
+                child: Container(
+                  height: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                       Container(
+                  height: 90,
+                  width: 100,
+                  child: Image.network(
+                    product.image,
+                    fit: BoxFit.fill,
+                  )),
+                        SizedBox(
+                          width: 320.0,
+                          child: RaisedButton(
+                            onPressed: () {},
+                            child: Text(
+                              (product.title),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            color: const Color(0xFF1BC0C5),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
     );
   }
 }
